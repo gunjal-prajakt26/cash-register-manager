733307d5f6d69d3effe33b306487950bf951d9ab
gunjal-prajakt26 <gunjal.prajakt951@gmail.com>
2022-01-21 12:44:11 +0530

    working complete

--- app.js
+++ app.js
@@ -2,42 +2,43 @@ var billAmount = document.querySelector(".bill-amount");
 var givenAmount = document.querySelector(".given-amount");
 var checkButton = document.querySelector(".btn-check");
 var errorMessage = document.querySelector(".error-msg");
-var numberOfNotes = document.querySelectorAll(".no-of-notes");
+var avilableNotes = document.querySelectorAll(".no-of-notes");
 
- numberOfNotes = [2000, 500, 100, 20, 10, 5, 1];
+var numberOfNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 // var amountOfBill = billAmount.target.value;
 // var amountGiven = givenAmount.value;
 
 checkButton.addEventListener("click",()=>{
-
+    errorMessage.style.display="none";
     if (billAmount.value > 0) {
 
-        if (givenAmount.value >= billAmount.value) {
-
-            
-
-            for (var i = 0; i < availabeNotes.length; i++) {
-                var checkAmount = givenAmount.value - billAmount.value;
-                var notes= Math.trunc(checkAmount / availabeNotes[i]);
-                var remainingAmount = checkAmount % availabeNotes[i];
-                console.log(availabeNotes+ notes);
-                // numberOfNotes[i].innerText = notes;
-            }
+        if (billAmount.value <= givenAmount.value) {
+            var checkAmount = givenAmount.value - billAmount.value;
+            checkOut(checkAmount);
 
         } else {
-            console.log("not-ok");
+            showMessage("Amount will be equal or greater than bill amount");
+            
         }
     } else {
         if (isNaN(billAmount.value)) {
-            errorMessage.innerText = "amount is only in numbers";
+            showMessage("Amount is only in numbers");
 
         } else {
-            errorMessage.innerText = "Amount can't be Negative";
+            showMessage("Amount will be gretaer than 0");
         }
     }
 });
 
-function calculate(notes) {
-    numberOfNotes.innerText = notes;
+function checkOut(amnt){
+    for (var i= 0; i < numberOfNotes.length; i++) {
+        var notes= Math.trunc(amnt / numberOfNotes[i]);
+        var amnt = amnt % numberOfNotes[i];
+        avilableNotes[i].innerText = notes;
+    }
+}
+function showMessage(msg) {
+    errorMessage.style.display="block";
+    errorMessage.innerText = msg;
 }
